a6d54a5a3564dda003b8a6c1d42b525d29811eab
Charles Scott <Charles.Scott@pointsmith.com>
2017-08-08 15:55:33 -0500

    Updates

--- Assets/Scripts/NumberWizards.cs
+++ Assets/Scripts/NumberWizards.cs
@@ -2,39 +2,51 @@ using UnityEngine;
 
 public class NumberWizards : MonoBehaviour
 {
+    public int min = 1;
+    public int max = 1000;
+    public int guess = 500;
 
     // Use this for initialization
-    void Start()
+    public void Start()
     {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        max++;
         print("Welcome to Number Wizard");
         print("Pick a number in your head, but don't tell me");
-
-        int max = 1000;
-        int min = 1;
 
         print("The highest number you can pick is " + max);
         print("The lowest number you can pick is " + min);
 
-        print("Is the number higher or lower than 500?");
+        print("Is the number higher or lower than " + guess + " ?");
         print("Up = for higher, down = lower, return = equals");
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            print("Up arrow pressed");
+            min = guess;
+            NextGuess();
         }
-
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            print("Down arrow pressed");
+            max = guess;
+            NextGuess();
         }
-
-        if (Input.GetKeyDown(KeyCode.Return))
+        else if (Input.GetKeyDown(KeyCode.Return))
         {
             print("I won!");
         }
+    }
+    public void NextGuess()
+    {
+        guess = (max + min) / 2;
+        print("Higher or lower than " + guess);
+        print("Up = for higher, down = lower, return = equals");
     }
 }
